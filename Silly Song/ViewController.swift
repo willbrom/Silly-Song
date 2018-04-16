8ba237b16fb6a4dcdf9a11ac08d9b3cc666a5b88
Ali Sajid <alisajid@alis-macbook-air.local>
2018-04-16 19:47:25 +0300

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Ali Sajid on 30/07/1439 AH.
+//  Copyright © 1439 Ali Sajid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Silly SongTests/Silly_SongTests.swift
@@ -0,0 +1,36 @@
+//
+//  Silly_SongTests.swift
+//  Silly SongTests
+//
+//  Created by Ali Sajid on 30/07/1439 AH.
+//  Copyright © 1439 Ali Sajid. All rights reserved.
+//
+
+import XCTest
+@testable import Silly_Song
+
+class Silly_SongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
